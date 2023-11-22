cae856a13d20bc1f176bedd8d95d7d2b7a24735f
gianlucaciralli <gianluca@ciralli.com>
2023-11-22 18:54:40 +0100

    Add files via upload
    First Build

---
+++ sample/script.js
@@ -0,0 +1,20 @@
+ClassicEditor
+	.create( document.querySelector( '.editor' ), {
+		// Editor configuration.
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( handleSampleError );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "to8gts7hz6dt-1s0ulpq7iies" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
---
+++ src/ckeditor.ts
@@ -0,0 +1,110 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Autosave } from '@ckeditor/ckeditor5-autosave';
+import { Bold, Italic, Strikethrough, Underline } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import type { EditorConfig } from '@ckeditor/ckeditor5-core';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontBackgroundColor, FontColor, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Mention } from '@ckeditor/ckeditor5-mention';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import {
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersEssentials,
+	SpecialCharactersText
+} from '@ckeditor/ckeditor5-special-characters';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Alignment,
+		Autoformat,
+		Autosave,
+		BlockQuote,
+		Bold,
+		Essentials,
+		FontBackgroundColor,
+		FontColor,
+		FontSize,
+		Heading,
+		HorizontalLine,
+		Indent,
+		Italic,
+		Link,
+		List,
+		ListProperties,
+		MediaEmbed,
+		Mention,
+		Paragraph,
+		PasteFromOffice,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersEssentials,
+		SpecialCharactersText,
+		Strikethrough,
+		Table,
+		TableToolbar,
+		TextTransformation,
+		Underline,
+		WordCount
+	];
+
+	public static override defaultConfig: EditorConfig = {
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'alignment',
+				'bold',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'outdent',
+				'indent',
+				'|',
+				'blockQuote',
+				'insertTable',
+				'undo',
+				'redo',
+				'fontColor',
+				'fontBackgroundColor',
+				'horizontalLine',
+				'specialCharacters',
+				'strikethrough',
+				'underline'
+			]
+		},
+		language: 'it',
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells'
+			]
+		}
+	};
+}
+
+export default Editor;
